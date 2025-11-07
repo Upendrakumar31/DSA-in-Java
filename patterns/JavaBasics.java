8510186895212c2e515372b2f761b92da57e34f1
upendrakumar <krupendra7725@gmail.com>
2025-11-07 22:44:05 +0530

    patterns code upload

---
+++ patterns/JavaBasics.java
@@ -0,0 +1,13 @@
+package patterns;
+
+import java.util.Scanner;
+
+//Checking the Prime Number 
+
+public class JavaBasics {
+    public static void main(String args[]){
+        for(int i = 0; i <= 5; i++){
+            System.out.println("i is " + i);
+        }
+ }      
+}
